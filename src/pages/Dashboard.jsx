eea74104f2472f3724a0173b9437f1514fef7195
CarolinaEM1 <20031299@itcelaya.edu.mx>
2026-04-27 20:42:00 -0600

    inicio

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,13 +7,6 @@ function Dashboard() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
-  const getValue = (obj, keys) => {
-    for (let key of keys) {
-      if (obj?.[key]) return obj[key];
-    }
-    return "No disponible";
-  };
-
   useEffect(() => {
     const fetchStudent = async () => {
       try {
@@ -21,16 +14,9 @@ function Dashboard() {
 
         console.log("RESPUESTA ESTUDIANTE:", response.data);
 
-        const data =
-          response.data?.message?.estudiante ||
-          response.data?.message ||
-          response.data?.estudiante ||
-          response.data?.data ||
-          response.data;
-
-        setStudent(data);
+        setStudent(response.data.data);
       } catch (err) {
-        console.log("ERROR ESTUDIANTE:", err.response?.data || err.message);
+        console.log("ERROR:", err.response?.data || err.message);
         setError("No se pudo cargar la información del estudiante.");
       } finally {
         setLoading(false);
@@ -64,36 +50,34 @@ function Dashboard() {
 
       <h1>Dashboard del estudiante</h1>
 
-      <div>
-        <h2>{getValue(student, ["nombre", "Nombre", "name", "full_name"])}</h2>
+      <p>
+        <strong>Nombre:</strong> {student.persona}
+      </p>
 
-        <p>
-          <strong>No. Control:</strong>{" "}
-          {getValue(student, [
-            "no_control",
-            "NoControl",
-            "numero_control",
-            "control",
-            "nocontrol",
-            "matricula",
-          ])}
-        </p>
+      <p>
+        <strong>No. Control:</strong> {student.numero_control}
+      </p>
 
-        <p>
-          <strong>Carrera:</strong>{" "}
-          {getValue(student, ["carrera", "Carrera", "career", "programa"])}
-        </p>
+      <p>
+        <strong>Correo:</strong> {student.email}
+      </p>
 
-        <p>
-          <strong>Correo:</strong>{" "}
-          {getValue(student, ["email", "correo", "Correo"])}
-        </p>
+      <p>
+        <strong>Semestre:</strong> {student.semestre}
+      </p>
 
-        <p>
-          <strong>Semestre:</strong>{" "}
-          {getValue(student, ["semestre", "Semestre", "semester"])}
-        </p>
-      </div>
+      <p>
+        <strong>Créditos acumulados:</strong> {student.creditos_acumulados}
+      </p>
+
+      <p>
+        <strong>Promedio ponderado:</strong>{" "}
+        {Number(student.promedio_ponderado).toFixed(2)}
+      </p>
+
+      <p>
+        <strong>Avance de carrera:</strong> {student.porcentaje_avance}%
+      </p>
     </div>
   );
 }
